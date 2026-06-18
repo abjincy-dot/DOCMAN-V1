37f63d1d94acd22bf68eff5a0ca16eb7fa513d1e
abjincy-dot <abjincy@gmail.com>
2026-06-18 06:52:53 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+const CACHE = 'docman-v219';
+const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res.ok) { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request, c)); }
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
