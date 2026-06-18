2b7c92e933f5d8d11890326b8a3b5a5cdf9ef725
abjincy-dot <abjincy@gmail.com>
2026-06-18 13:53:55 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'docman-v4';
+const CACHE = 'docman-v5';
 const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
