1b6b08d711bc05a40ff059c182cfec00a466e12b
abjincy-dot <abjincy@gmail.com>
2026-06-28 00:04:54 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = '1.0.0';
-const CACHE = 'docman-v96';
+const CACHE = 'docman-v97';
 const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json', './Images/settings-tray.png', './Images/settings-neon.png'];
 
 self.addEventListener('install', e => {
