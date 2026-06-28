edf5d057a05843d46932d646f95ea5e95b1eefe1
abjincy-dot <abjincy@gmail.com>
2026-06-28 11:24:59 +0400

    Add jszip.min.js to service worker assets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const APP_VERSION = '1.0.0';
 const CACHE = 'docman-v100';
-const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json', './Images/settings-tray.png', './Images/settings-neon.png'];
+const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json', './Images/settings-tray.png', './Images/settings-neon.png', './vendor/jszip/jszip.min.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
